873dd9e9fb9524ad00b8a1b2395aeee545c71e15
Mason Balbera <mhbalbera@gmail.com>
2019-07-29 18:27:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,34 @@
 # Your Code Here
+
+def map(arr, &prc)
+  
+  x = 0
+  while x < arr.length 
+  arr[x] = prc.call(arr[x])
+  x += 1 
+  end   
+  return arr
+end 
+
+def reduce(arr, acc = 0, &prc)
+   x = 0
+    if prc == {|memo, n| memo || n}
+      if arr.any?{|ele| ele}
+           return true 
+      else
+          return false 
+      end
+      
+    else
+     
+      while x < arr.length 
+        acc = prc.call(acc, arr[x])
+        x += 1 
+      end  
+      return acc
+    end
+    
+    
+
+  
+end
